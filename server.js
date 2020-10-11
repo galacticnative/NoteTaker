7d9b26531b017c4908b7816daf6f552ea9fbf797
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-10-11 15:21:46 -0700

    set up get id

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { database } = require('./data/db.json')
 
+//to allow Heroku to convert server to port 80
 const PORT = process.env.PORT || 3001;
 const app = express();
 //const notesRoutes = require('./routes/notesRoutes');
@@ -14,11 +15,39 @@ app.use(express.json());
 app.use(express.static('public'));
 //app.use('/api', notesRoutes);
 
+function filterByQuery(query, notesArray) {
+    let filteredResults = notesArray;
+    if (query.title) {
+      filteredResults = filteredResults.filter(database => database.title === query.title);
+    }
+    if (query.text) {
+      filteredResults = filteredResults.filter(database => database.text === query.text);
+    }
+    return filteredResults;
+}
+
+//brings up single object instead of an array
+function findById(id, notesArray) {
+    const result = notesArray.filter(database => database.id === id)[0];
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+}
+
 app.get('/api/database', (req, res) => {
     let results = database
-    console.log(req.query)
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
     res.json(results);
 })
+
+app.get('/api/database/:id', (req, res) => {
+    const result = findById(req.params.id, database);
+      res.json(result);
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
